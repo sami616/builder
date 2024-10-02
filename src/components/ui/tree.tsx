10551574019ebdc11d6f3015818ce894b9469a97
sami616 <samuel.resua@bp.com>
2024-10-02 16:11:56 +0100

    wip

--- src/components/ui/tree.tsx
+++ src/components/ui/tree.tsx
@@ -1,30 +1,53 @@
-import { HTMLProps, ReactNode, RefObject, useEffect, useRef, useState } from 'react'
+import { Dispatch, HTMLProps, RefObject, SetStateAction } from 'react'
+import { Collapsible, CollapsibleContent, CollapsibleTrigger } from './collapsible'
+import { ChevronDown, ChevronRight } from 'lucide-react'
+import { DropIndicator } from '../editor/DropIndicator'
+import { DragPreview } from '../editor/DragPreview'
+import { Edge } from '@/hooks/useDrop'
+import { ReactNode } from '@tanstack/react-router'
 
 export function Tree(props: {
-  labelTrigger?: boolean
-  items: JSX.Element[]
-  label: ReactNode
-  defaultOpen?: boolean
-  openIcon?: JSX.Element
-  closedIcon?: JSX.Element
+  drop?: { ref: RefObject<HTMLLIElement>; edge: Edge }
+  open: boolean
+  setOpen: Dispatch<SetStateAction<boolean>>
+  li: HTMLProps<HTMLLIElement>
+  item: ReactNode
+  collapsibleItems: JSX.Element[]
+  isHovered: boolean
+  isDragging: boolean
+  active: boolean
 }) {
-  const [isOpen, setIsOpen] = useState(props.defaultOpen ?? false)
-
-  const Label = props.labelTrigger ? (
-    <button className="group flex gap-2 items-center" onClick={() => setIsOpen(!isOpen)}>
-      {isOpen ? props.openIcon : props.closedIcon}
-      {props.label}
-    </button>
-  ) : (
-    <div className="group flex gap-2 items-center">
-      <button onClick={() => setIsOpen(!isOpen)}>{isOpen ? props.openIcon : props.closedIcon}</button>
-      {props.label}
-    </div>
-  )
   return (
-    <div className="grid text-sm p-2 gap-2">
-      {Label}
-      {isOpen && <ul className="ml-2 border-l">{props.items}</ul>}
-    </div>
+    <Collapsible asChild open={props.open} onOpenChange={props.setOpen}>
+      <li
+        {...props.li}
+        ref={props.drop?.ref}
+        // data-component="BlockLayerItem"
+        // data-drop-id={`block-${blockGet.data.id}`}
+        className={[
+          'select-none',
+          'grid',
+          'gap-2',
+          'p-2',
+          'text-sm',
+          props.isHovered && 'bg-gray-100',
+          props.isDragging ? 'opacity-50' : 'opacity-100',
+          props.active && 'ring-inset ring-2 ring-emerald-500',
+        ].join(' ')}
+      >
+        <div className="w-full flex gap-2 items-center">
+          <CollapsibleTrigger className="cursor-pointer shrink-0 opacity-40 hover:opacity-100" asChild>
+            {props.open ? <ChevronDown size={16} /> : <ChevronRight size={16} />}
+          </CollapsibleTrigger>
+          {props.item}
+        </div>
+        <CollapsibleContent asChild>
+          <ul className="pl-2 ml-2 border-l border-dashed">{props.collapsibleItems}</ul>
+        </CollapsibleContent>
+        {/* stuff  */}
+        {props.drop && <DropIndicator closestEdge={props.drop.edge} variant="horizontal" />}
+        {/* <DragPreview dragPreviewContainer={dragPreviewContainer}>{blockGet.data.name}</DragPreview> */}
+      </li>
+    </Collapsible>
   )
 }
