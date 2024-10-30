e17a61511bc112902d4e01b9181326614e84d516
sami616 <samuel.resua@bp.com>
2024-10-30 15:22:45 +0000

    wip

--- src/hooks/use-block-delete-many.ts
+++ src/hooks/use-block-delete-many.ts
@@ -14,22 +14,19 @@ export function useBlockDeleteMany() {
     mutationFn: async (args: Args) => {
       const removeList = []
       const updateList = new Map<number, Args['entries'][number]['parent']['node']>() // Map to store each unique cloned parent node
+      // sort to process deletions from the highest index to the lowest,
+      // you avoid shifting indices that haven’t yet been processed,
+      // as deletions will only affect subsequent indices in the array.
+      // This ensures that each index value still accurately points to the correct element.
+      const sortedEntries = args.entries.sort((a, b) => b.index - a.index)
 
-      for (const entry of args.entries) {
+      for (const entry of sortedEntries) {
         const tree = await context.getTree({ root: { store: 'blocks', id: entry.blockId }, entries: [] })
         removeList.push(...tree)
 
-        let clonedParentNode
-        if (updateList.has(entry.parent.node.id)) {
-          // Use existing cloned parent if already processed
-          clonedParentNode = updateList.get(entry.parent.node.id)
-        } else {
-          // Clone and store if seeing this parent for the first time
-          clonedParentNode = structuredClone(entry.parent.node)
-          updateList.set(entry.parent.node.id, clonedParentNode)
-        }
-
+        const clonedParentNode = updateList.get(entry.parent.node.id) ?? structuredClone(entry.parent.node)
         clonedParentNode?.slots[entry.parent.slot].splice(entry.index, 1)
+        updateList.set(entry.parent.node.id, clonedParentNode)
       }
 
       await Promise.all([context.removeMany({ entries: removeList }), context.updateMany({ entries: Array.from(updateList.values()) })])
